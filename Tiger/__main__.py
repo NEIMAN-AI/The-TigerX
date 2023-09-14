a790f00d7ff51cf117f36fe21f28f46e6ccc9bc5
Team Neiman <136953008+NEIMAN-AI@users.noreply.github.com>
2023-09-15 02:13:01 +0530

    Create __main__.py

---
+++ Tiger/__main__.py
@@ -0,0 +1,26 @@
+import asyncio
+import importlib
+from pyrogram import Client, idle
+from Tiger.helper import join
+from Tiger.modules import ALL_MODULES
+from Tiger import clients, app, ids
+
+async def start_bot():
+    await app.start()
+    print("LOG: ғᴏᴜɴᴅᴇᴅ ʙᴏᴛ ᴛᴏᴋᴇɴ ʙᴏᴏᴛɪɴɢ..")
+    for all_module in ALL_MODULES:
+        importlib.import_module("Zaid.modules" + all_module)
+        print(f"sᴜᴄᴄᴇssғᴜʟʟʏ ɪᴍᴘᴏʀᴛᴇᴅ {all_module} ⚡")
+    for cli in clients:
+        try:
+            await cli.start()
+            ex = await cli.get_me()
+            await join(cli)
+            print(f"sᴛᴀʀᴛᴇᴅ {ex.first_name} ⚡")
+            ids.append(ex.id)
+        except Exception as e:
+            print(f"{e}")
+    await idle()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
