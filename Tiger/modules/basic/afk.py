6937849a416606c2350e56ceb1dda3f5b054c257
Team Neiman <136953008+NEIMAN-AI@users.noreply.github.com>
2023-09-17 00:24:18 +0530

    Update afk.py

--- Tiger/modules/basic/afk.py
+++ Tiger/modules/basic/afk.py
@@ -6,7 +6,6 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 
 from Tiger.helper.PyroHelpers import GetChatID, ReplyCheck
-from Tiger.modules.help import add_command_help
 
 AFK = False
 AFK_REASON = ""
