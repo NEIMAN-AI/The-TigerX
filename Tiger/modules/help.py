af37588d95bd497604052f9ea917d718edad6677
Team Neiman <136953008+NEIMAN-AI@users.noreply.github.com>
2023-09-28 17:24:59 +0530

    Update help.py

--- Tiger/modules/help.py
+++ Tiger/modules/help.py
@@ -21,5 +21,5 @@ async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
 async def module_help(client: Client, message: Message):
     mg = await edit_or_reply(
         message,
-        "╭✠╼━━━❰𝗧𝗵𝗲-𝗧𝗶𝗴𝗲𝗿𝗫❱━━━✠╮\n│» 𝗮𝗳𝗸\n│» 𝗺𝘂𝘀𝗶𝗰\n│» 𝗹𝘆𝗿𝗶𝗰𝘀\n│» 𝗴𝗼𝗼𝗴𝗹𝗲\n│» 𝗰𝗹𝗼𝗻𝗲\n│» 𝘀𝗽𝗮𝗺\n│» 𝗽𝗶𝗻𝗴\n│» 𝗮𝗹𝗶𝘃𝗲\n│» 𝘀𝘁𝗶𝗰𝗸𝗲𝗿𝘀\n│» 𝘀𝗮𝗻𝗴𝗺𝗲𝘁𝗮\n│» 𝗽𝗿𝗼𝗳𝗶𝗹𝗲\n│» 𝘁𝗲𝘅𝘁\n│» 𝗲𝗺𝗼𝗷𝗶\n│» 𝗱𝗺𝘀𝗽𝗮𝗺\n│» 𝘁𝗮𝗴𝗮𝗹𝗹\n│» 𝘁𝗲𝗹𝗲𝗴𝗿𝗮𝗽𝗵\n│» 𝘃𝗰 𝘁𝗼𝗼𝗹𝘀\n│» 𝗷𝗼𝗶𝗻\n│» 𝗹𝗲𝗮𝘃𝗲\n│» 𝗜𝗻𝘃𝗶𝘁𝗲\n╰✠╼━━━━━━❖━━━━━━━✠╯"
+        "╭✠╼━━━❰𝗧𝗵𝗲-𝗧𝗶𝗴𝗲𝗿𝗫❱━━━✠╮\n[ʜᴇʟᴘ ᴍᴇɴᴜ](t.me/DETECTED_09)"
     )
